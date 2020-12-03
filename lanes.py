e831cdf06b58a536bc87e2323f8216245e69853d
Iulian Oana <iulian.andreioana@gmail.com>
2020-12-03 21:37:46 +0000

    64.1. Hough Transform II

--- lanes.py
+++ lanes.py
@@ -14,13 +14,27 @@ def region_of_interest(image):
     polygons = np.array([
     [(200, height), (1100, height), (550, 250)]], dtype = np.int32)
     mask = np.zeros_like(image, dtype = np.uint8)
+    print(mask)
     cv2.fillPoly(mask, polygons, (255,255,255))
-    # Apply bitwise and onto the image using the mask.
     masked_image = cv2.bitwise_and(mask, image)
     return masked_image
 
 lane_image = np.copy(image)
 canny = canny(lane_image)
-masked_image = region_of_interest(canny)
-cv2.imshow("result", masked_image)
+cropped_image = region_of_interest(canny)
+"""
+Draws the Hough lines
+arg1: the image cropped
+arg2: the bin size that collect votes on the number of lines intersections
+based on the number of points detected in the cartesian space
+arg3: precision in degrees: one degree to radians = pi/180 = 1
+arg4: lower threshold: the minimum number of intersections needed to
+detect the line
+arg5: placeholder array to be passed in
+arg6: the lenght of the line in px accepted in the output
+arg6: max line gap: the maximum distance in px between segmented lines
+which we'll allow to be connected into a single one.
+"""
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLenght=40, maxLineGap=5)
+cv2.imshow("result", cropped_image)
 cv2.waitKey(0)
