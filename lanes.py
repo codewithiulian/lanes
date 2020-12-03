a9e0776f0913350ff14f7ca755934f7a6b57d5aa
Iulian Oana <iulian.andreioana@gmail.com>
2020-12-03 10:54:02 +0000

    56.1 - Loading Image

--- lanes.py
+++ lanes.py
@@ -1,5 +1,9 @@
-import cv2
+import cv2 # OpenCV
+import numpy as np
 
+# Save the input image into a multidimensional array
 image = cv2.imread('test_image.jpg')
-cv2.imshow('result', image)
-cv2.waitKey(0)
+
+cv2.imshow('result', image) # Show the image using OpenCV
+# This function is necessary for showing the display window
+cv2.waitKey(0) # The argument is the milliseconds, 0 is infinite
